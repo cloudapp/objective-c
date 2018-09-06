6802256323d6cd5e6c7f3ce0e5a1c60a20167bdc
Héctor Cuevas Morfín <poncho_nov@hotmail.com>
2018-09-06 15:41:36 -0500

    Cloudappsdk example changed

--- CloudAppSDK/ViewController.swift
+++ CloudAppSDK/ViewController.swift
@@ -8,44 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLAPIEngineDelegate {
+    
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        CLAPIEngine.shared().delegate = self;
-
-       // engine.getAccountToken("GetTokenAccount")
-        
-        //engine.getItemListStarting(atPage: 1, itemsPerPage: 5, userInfo: nil)
+        CLAPIEngine.shared().delegate = self
+        imagePicker.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
-
+        // CLAPIEngine.sharedInstance()?.signUp()
+        
     }
-
-    func itemListRetrievalSucceeded(_ items: [Any]!, connectionIdentifier: String!, userInfo: Any!) {
-        for item in items {
-            print(item)
-        }
-    }
-    
-    @IBAction func openLoginView() {
+    @IBAction func login(_ sender: Any) {
         CLAPIEngine.shared().logIn()
     }
+    @IBAction func getitems(_ sender: Any) {
+        CLAPIEngine.shared().getItemListStarting(atPage: 1, itemsPerPage: 5, userInfo: nil)
+    }
     
-    @IBAction func getDrops() {
-        //CLAPIEngine.sharedInstance()?.getItemListStarting(atPage: 1, itemsPerPage: 10, userInfo: nil)
+    @IBAction func getVideosTapped(_ sender: Any) {
         CLAPIEngine.shared().getItemListStarting(atPage: 1, ofType: CLWebItemTypeVideo, itemsPerPage: 10, showOnlyItemsInTrash: false, userInfo: nil)
     }
     
-}
-
-extension ViewController: CLAPIEngineDelegate {
+    
+    
     func itemListRetrievalSucceeded(_ items: [CLWebItem]!, connectionIdentifier: String!, userInfo: Any!) {
         for item in items {
-            print(item.name)
+            print("name: ",item.name, "type", item.type)
         }
+        print("\n")
     }
+    
+    
+    @IBAction func showPicker(_ sender: Any) {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+    func fileUploadDidSucceed(withResultingItem item: CLWebItem!, connectionIdentifier: String!, userInfo: Any!) {
+        print(item.url)
+    }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let imageURL = info[UIImagePickerController.InfoKey.imageURL] as? URL else { return }
+        
+        CLAPIEngine.shared().uploadFile(withName: imageURL.lastPathComponent, atPathOnDisk: imageURL.path, options: nil, userInfo: nil)
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func fileUploadDidProgress(_ percentageComplete: CGFloat, connectionIdentifier: String!, userInfo: Any!) {
+        // print(percentageComplete)
+    }
+    
+}
